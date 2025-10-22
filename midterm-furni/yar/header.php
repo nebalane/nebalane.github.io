b3b14c38b5b103e41b944e69e0510a2f0004fc46
nebalane <nebalane.student@asiancollege.edu.ph>
2025-10-22 08:17:53 +0800

    Add files via upload

---
+++ midterm-furni/yar/header.php
@@ -0,0 +1,6 @@
+<header>
+    <div><a href="home.php">Home</a></div>
+    <div><a href="profile.php">Profile</a></div>
+    <div><a href="login.php">Login</a></div>
+    <div><a href="signup.php">Signup</a></div>
+</header>
---
+++ midterm-furni/yar/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title> 
+</head>
+<body>
+    <?php require_once 'header.php';?>
+    <?php require 'footer.php';?>
+</body>
+</html>
